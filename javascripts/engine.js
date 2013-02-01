7e31feaf0f15ad5fa705f7aca4871514bda705b0
John Smith <gshubham@dn5213ie.sunet>
2013-01-31 21:41:23 -0800

    Draw working!

--- javascripts/engine.js
+++ javascripts/engine.js
@@ -4,41 +4,31 @@ $(document).ready(function() {
 		canvas: "#canvas"
 	});
 
-	var pentagon = canvas.display.polygon({
-		x: canvas.width / 1.5,
-		y: canvas.width / 1.5,
-		sides: 5,
-		radius: 60,
-		fill: "#18a"
-	});
 
 	var line = canvas.display.line({
-		start: { x: 80, y: 60 },
-		end: { x: 280, y: 170 },
-		stroke: "20px #0aa",
+		start: { x: 30, y: 300 },
+		end: { x: 480, y: 300 },
+		stroke: "5px #0aa",
 		cap: "round"
 	});
 
-	canvas.addChild(pentagon);
-
 	var dragOptions = { changeZindex: true };
 
-	pentagon.dragAndDrop(dragOptions);
-
 	canvas.setLoop(function () {
-		pentagon.rotation--;
+//		pentagon.rotation--;
 	});
 
 	$("#draw-ground").click(function() {
 
 		canvas.addChild(line);
 
-		if (canvas.timeline.running) {
+
+		/*if (canvas.timeline.running) {
 			canvas.timeline.stop();
 		} 
 		else {
 			canvas.timeline.start();
-		}
+		}*/
 	});
 
 });
